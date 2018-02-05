6d40bea50daa28d56e67a0081a513ed6dda6edd8
Marcus Rands <mrands@hotmail.co.uk>
2018-02-05 15:49:33 +0000

    extraced code for changing direction index in to seperate private method. Tests still passing

--- lib/kitten_finder.rb
+++ lib/kitten_finder.rb
@@ -5,12 +5,12 @@ class Kitten_finder# works out the location of kittens based on directions passe
   attr_reader :directions, :current_direction
 
 
-  def initialize(api = Forensics_api.new, current_direction = 'North')#initialize kitten_finder class with an instance of api
-    @api = api #instance of api
-    @directions = ["North", "East", "South", "West"]#array of four possible directions a searcher can be facing
-    @current_direction = current_direction #starting direction . Hard coded
-    @x_location = 0 #start location on x-axis
-    @y_location = 0 #start location on y-axis
+  def initialize(api = Forensics_api.new, current_direction = 'North')
+    @api = api
+    @directions = ["North", "East", "South", "West"]
+    @current_direction = current_direction
+    @x_location = 0
+    @y_location = 0
   end
 
   def locate_kittens
@@ -20,15 +20,15 @@ class Kitten_finder# works out the location of kittens based on directions passe
     current_location
   end
 
-  def confirm_kitten_location # makes a call to the api class to call submit location and return confirmation.
+  def confirm_kitten_location
     @api.submit_kitten_location(@x_location, @y_location)
   end
 
-  def current_location #getter method to return the x and y coordinates in a single array.
+  def current_location
     [@x_location, @y_location]
   end
 
-  def move_forward # Instructs searcher to move forward in the direction they are currently facing.
+  def move_forward
     case @current_direction
     when "North"
       @y_location += 1
@@ -41,22 +41,26 @@ class Kitten_finder# works out the location of kittens based on directions passe
     end
   end
 
-  def change_direction_right #changes direction of searcher to right when instructed.
+  def change_direction_right
     index = get_direction_index
     index < 3 ? index += 1 : index = 0
-    @current_direction = @directions[index]
+    change_direction_index(index)
   end
 
-  def change_direction_left# changees direction of searcher to left when instructed.
+  def change_direction_left
     index = get_direction_index
     index > 0 ? index -= 1 : index = 3
-    @current_direction = @directions[index]
+    change_direction_index(index)
   end
 
   private
 
   def get_direction_index
     @directions.find_index(@current_direction)
+  end
+
+  def change_direction_index(index)
+    @current_direction = @directions[index]
   end
 
   def instruction_guide(instruction)
